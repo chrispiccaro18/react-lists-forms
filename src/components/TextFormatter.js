f3ac18487679162b1b9c19a93f98cdbc1fd6b9ef
Chris Piccaro <chrispiccaro18@gmail.com>
2019-05-15 17:03:19 -0700

    added figlet

--- src/components/TextFormatter.js
+++ src/components/TextFormatter.js
@@ -1,29 +1,47 @@
 import React, { PureComponent } from 'react';
+import figlet from 'figlet';
 
 export default class TextFormatter extends PureComponent {
   state = {
     text: '',
-    color: '#000000'
+    color: '#000000',
+    font: 'Banner',
+    formattedFont: ''
   }
 
-  handleTextChange = ({ target }) => {
-    this.setState({ text: target.value });
+  handleChange = ({ target }) => {
+    this.setState({ [target.name]: target.value }, () => {
+      this.figletText();
+    });
   }
-  
-  handleColorChange = ({ target }) => {
-    this.setState({ color: target.value });
+
+  figletText = () => {
+    const { text, font } = this.state;
+    figlet.text(text, { font: font }, (err, result) => {
+      if(err) {
+        // eslint-disable-next-line no-console
+        console.log(err);
+        return;
+      }
+      this.setState({ formattedFont: result });
+    });
   }
 
   render() {
-    const { text, color } = this.state;
+    const { text, color, formattedFont, font } = this.state;
     const pTextStyle = {
       color: color
     };
+    const figletOptions = ['Alpha', 'Avatar', 'Banner', 'Alligator', 'Linux'].map(font => (
+      <option key={font} value={font}>{font}</option>
+    ));
+
     return (
       <>
-        <input type="text" name="text" value={text} onChange={this.handleTextChange} />
-        <input type="color" name="color" value={color} onChange={this.handleColorChange} />
-        <p style={pTextStyle}>{text}</p>
+        <input type="text" name="text" value={text} onChange={this.handleChange} />
+        <select name="font" value={font} onChange={this.handleChange}>{figletOptions}</select>
+        <input type="color" name="color" value={color} onChange={this.handleChange} />
+        <pre style={pTextStyle}>{formattedFont}</pre>
       </>
     );
   }
